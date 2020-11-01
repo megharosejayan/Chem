1c5c7aa5c495debcc298b61b76a9a8668c908e32
Aman-zishan <amanzishan.az@gmail.com>
2020-11-01 22:26:54 +0530

    add bottom to top buttom

--- wp-content/themes/sayidan/js/custom/scroller.js
+++ wp-content/themes/sayidan/js/custom/scroller.js
@@ -1,16 +1,12 @@
+var scrollToTopBtn = document.getElementById("scrollToTopBtn")
+var rootElement = document.documentElement
 
-var btn = $('#button');
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
 
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 30) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
-
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
-
+scrollToTopBtn.addEventListener("click", scrollToTop)
